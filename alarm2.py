6a93e72feb09ac59d5daada7946bb2bc5a4b04c9
Arief Septian <18215013@std.stei.itb.ac.id>
2019-04-30 21:40:16 +0700

    tambahan

--- alarm2.py
+++ alarm2.py
@@ -32,51 +32,68 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
 
         self.alarm_h = 0
-        self.alarm_m = 0
-        
-        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+	self.alarm_m = 0
+	
+	MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(676, 439)
-        
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.gridLayout = QtGui.QGridLayout(self.centralwidget)
         self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+	
+	current_time = QtCore.QTime()
+        self.Set_Time = QtGui.QTimeEdit(self.centralwidget)
+        self.Set_Time.setObjectName(_fromUtf8("Set_Time"))
+	self.Set_Time.setTime(current_time.currentTime())
+        self.gridLayout.addWidget(self.Set_Time, 2, 0, 1, 1)
+
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout.addWidget(self.pushButton, 3, 0, 1, 1)
+
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayout.addWidget(self.label_2, 2, 1, 1, 2)
+
+        self.Time_LCD = QtGui.QLCDNumber(self.centralwidget)
+        self.Time_LCD.setObjectName(_fromUtf8("Time_LCD"))
+	self.Time_LCD.setDigitCount(8)
+        self.Time_LCD.display(strftime("%H"+":"+"%M"+":"+"%S")) #get time from Pi and display it 
+
+        self.gridLayout.addWidget(self.Time_LCD, 1, 0, 1, 3)
         self.label = QtGui.QLabel(self.centralwidget)
-        
         font = QtGui.QFont()
         font.setPointSize(14)
         self.label.setFont(font)
         self.label.setObjectName(_fromUtf8("label"))
-        self.gridLayout.addWidget(self.label, 3, 0, 1, 1)
-        
-        self.Time_LCD = QtGui.QLCDNumber(self.centralwidget)
-        self.Time_LCD.setObjectName(_fromUtf8("Time_LCD"))
-        self.Time_LCD.setDigitCount(8)
-        self.Time_LCD.display(strftime("%H"+":"+"%M"+":"+"%S")) #get time from Pi and display it 
-        self.gridLayout.addWidget(self.Time_LCD, 1, 0, 1, 3)
+        self.gridLayout.addWidget(self.label, 5, 0, 1, 1)
 
-        self.timer = QtCore.QTimer(MainWindow)
-        self.timer.timeout.connect(self.Time)
-        self.timer.start(1000)
-        
-        current_time = QtCore.QTime()
-        self.Set_Time = QtGui.QTimeEdit(self.centralwidget)
-        self.Set_Time.setObjectName(_fromUtf8("Set_Time"))
-        self.Set_Time.setTime(current_time.currentTime())
-        self.gridLayout.addWidget(self.Set_Time, 2, 0, 1, 1)
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setText(_fromUtf8(""))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.gridLayout.addWidget(self.pushButton_2, 3, 1, 1, 1)
 
+        self.pushButton_3 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_3.setText(_fromUtf8(""))
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.gridLayout.addWidget(self.pushButton_3, 3, 2, 1, 1)
+
+        self.pushButton_4 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_4.setText(_fromUtf8(""))
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
+        self.gridLayout.addWidget(self.pushButton_4, 4, 1, 1, 1)
+
+        self.pushButton_5 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_5.setText(_fromUtf8(""))
+        self.pushButton_5.setObjectName(_fromUtf8("pushButton_5"))
+        self.gridLayout.addWidget(self.pushButton_5, 4, 2, 1, 1)
+	MainWindow.setCentralWidget(self.centralwidget)
         
-        self.pushButton = QtGui.QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.pushButton.clicked.connect(self.button_pressed)
-        self.gridLayout.addWidget(self.pushButton, 2, 1, 1, 1)
-        MainWindow.setCentralWidget(self.centralwidget)
-        
-        self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 676, 21))
+	self.menubar = QtGui.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 676, 25))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
-        
+
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
         MainWindow.setStatusBar(self.statusbar)
--- ui_alarm.py
+++ ui_alarm.py
@@ -2,70 +2,102 @@
 
 # Form implementation generated from reading ui file 'ui_alarm.ui'
 #
-# Created by: PyQt5 UI code generator 5.10.1
+# Created by: PyQt4 UI code generator 4.12.1
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        
+	self.alarm_h = 0
+	self.alarm_m = 0
+	
+	MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(676, 439)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
-        self.Set_Time = QtWidgets.QTimeEdit(self.centralwidget)
-        self.Set_Time.setObjectName("Set_Time")
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+	
+	current_time = QtCore.QTime()
+        self.Set_Time = QtGui.QTimeEdit(self.centralwidget)
+        self.Set_Time.setObjectName(_fromUtf8("Set_Time"))
+	self.Set_Time.setTime(current_time.currentTime())
         self.gridLayout.addWidget(self.Set_Time, 2, 0, 1, 1)
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setObjectName("pushButton")
+
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
         self.gridLayout.addWidget(self.pushButton, 3, 0, 1, 1)
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setObjectName("label_2")
+
+        self.label_2 = QtGui.QLabel(self.centralwidget)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
         self.gridLayout.addWidget(self.label_2, 2, 1, 1, 2)
-        self.Time_LCD = QtWidgets.QLCDNumber(self.centralwidget)
-        self.Time_LCD.setObjectName("Time_LCD")
+
+        self.Time_LCD = QtGui.QLCDNumber(self.centralwidget)
+        self.Time_LCD.setObjectName(_fromUtf8("Time_LCD"))
+	self.Time_LCD.setDigitCount(8)
+        self.Time_LCD.display(strftime("%H"+":"+"%M"+":"+"%S")) #get time from Pi and display it 
+
         self.gridLayout.addWidget(self.Time_LCD, 1, 0, 1, 3)
-        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label = QtGui.QLabel(self.centralwidget)
         font = QtGui.QFont()
         font.setPointSize(14)
         self.label.setFont(font)
-        self.label.setObjectName("label")
+        self.label.setObjectName(_fromUtf8("label"))
         self.gridLayout.addWidget(self.label, 5, 0, 1, 1)
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setText("")
-        self.pushButton_2.setObjectName("pushButton_2")
+
+        self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_2.setText(_fromUtf8(""))
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
         self.gridLayout.addWidget(self.pushButton_2, 3, 1, 1, 1)
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setText("")
-        self.pushButton_3.setObjectName("pushButton_3")
+
+        self.pushButton_3 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_3.setText(_fromUtf8(""))
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
         self.gridLayout.addWidget(self.pushButton_3, 3, 2, 1, 1)
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setText("")
-        self.pushButton_4.setObjectName("pushButton_4")
+
+        self.pushButton_4 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_4.setText(_fromUtf8(""))
+        self.pushButton_4.setObjectName(_fromUtf8("pushButton_4"))
         self.gridLayout.addWidget(self.pushButton_4, 4, 1, 1, 1)
-        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_5.setText("")
-        self.pushButton_5.setObjectName("pushButton_5")
+
+        self.pushButton_5 = QtGui.QPushButton(self.centralwidget)
+        self.pushButton_5.setText(_fromUtf8(""))
+        self.pushButton_5.setObjectName(_fromUtf8("pushButton_5"))
         self.gridLayout.addWidget(self.pushButton_5, 4, 2, 1, 1)
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
+	MainWindow.setCentralWidget(self.centralwidget)
+        
+	self.menubar = QtGui.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 676, 25))
-        self.menubar.setObjectName("menubar")
+        self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
+
+        self.statusbar = QtGui.QStatusBar(MainWindow)
+        self.statusbar.setObjectName(_fromUtf8("statusbar"))
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "Set Alarm"))
-        self.label_2.setText(_translate("MainWindow", "Soal"))
-        self.label.setText(_translate("MainWindow", "Your next Alarm is at : "))
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.pushButton.setText(_translate("MainWindow", "Set Alarm", None))
+        self.label_2.setText(_translate("MainWindow", "Soal", None))
+        self.label.setText(_translate("MainWindow", "Your next Alarm is at : ", None))
 
