8efcec1f854870ab25aaf58c5ed666e3576c1cc3
Arief Septian <18215013@std.stei.itb.ac.id>
2019-05-01 16:50:51 +0700

    BISMILLAH

--- alarm3.py
+++ alarm3.py
@@ -9,14 +9,18 @@ import sys
 import time
 import os
 import random
+import subprocess
 
-from PyQt4 import QtCore, QtGui #PyQt4 is used for designing the GUI
-from espeak import espeak #text to speech sonversion
+
+from PyQt4 import QtCore,QtGui #PyQt4 is used for designing the GUI
+#from PyQt4.QtCore import *
+#from PyQt4.QtGui import *
+#from espeak import espeak #text to speech sonversion
 from time import strftime # To get time from Raspberry pi
 
 #Code from Qt Designer
 try:
-    _fromUtf8 = QtCore.QString.fromUtf8
+    _fromUtf8 = QtCore.QtString.fromUtf8
 except AttributeError:
     def _fromUtf8(s):
         return s
@@ -29,10 +33,27 @@ except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
 
+                
 class Ui_MainWindow(object):
+ 
     def setupUi(self, MainWindow):
         self.alarm_h = 0
         self.alarm_m = 0
+        self.pil1 = 0
+        self.pil2 = 0
+        self.pil3 = 0
+        self.pil4 = 0
+        self.input = 0
+        self.solution = 0
+        self.user_solution = 0
+        self.sol1 = 0
+        self.sol2 = 0
+        self.sol3 = 0
+        self.sol4 = 0
+        self.btn1pushed = False
+        self.btn2pushed = False
+        self.btn3pushed = False
+        self.btn4pushed = False
     	
     	MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.resize(480, 320)
@@ -51,11 +72,6 @@ class Ui_MainWindow(object):
     	self.Set_Time.setTime(current_time.currentTime())
         self.gridLayout.addWidget(self.Set_Time, 2, 0, 1, 1)
 
-        self.pushButton = QtGui.QPushButton(self.centralwidget)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.pushButton.clicked.connect(self.button_pressed)
-    	self.gridLayout.addWidget(self.pushButton, 3, 0, 1, 1)
-
         self.label_2 = QtGui.QLabel(self.centralwidget)
         self.label_2.setObjectName(_fromUtf8("label_2"))
         self.gridLayout.addWidget(self.label_2, 2, 1, 1, 2)
@@ -73,10 +89,18 @@ class Ui_MainWindow(object):
         self.label.setObjectName(_fromUtf8("label"))
         self.gridLayout.addWidget(self.label, 5, 0, 1, 1)
 
+
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.pushButton.clicked.connect(self.button_pressed)
+        self.gridLayout.addWidget(self.pushButton, 3, 0, 1, 1)
+
         self.pushButton_2 = QtGui.QPushButton(self.centralwidget)
         self.pushButton_2.setText(_fromUtf8(""))
         self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.pushButton_2.clicked.connect(self.button_pressed_1)
+        QtCore.QObject.connect(self.pushButton_2,QtCore.SIGNAL("clicked()"),self.button_pressed_1)
+        #self.pushButton_2.clicked.connect(self.button_pressed_1)
+
         self.gridLayout.addWidget(self.pushButton_2, 3, 1, 1, 1)
 
         self.pushButton_3 = QtGui.QPushButton(self.centralwidget)
@@ -120,69 +144,9 @@ class Ui_MainWindow(object):
         self.label_2.setText(_translate("MainWindow", "Soal", None))
         self.pushButton.setText(_translate("MainWindow", "Set Alarm", None))
 
-    ef get_user_solution(self):
+    #def get_user_solution(self):
         
-        self.sol1 = self.solution
-        self.sol2 = self.solution + random.randrange(1,10)
-        self.sol3 = self.solution - random.randrange(1,10)
-        self.sol4 = self.solution - random.randrange(1,10)
-        while (self.sol4 == self.sol3 ):
-            self.sol4 = self.solution - random.randrange(1,10)
-            pil1 = random.randrange(1,5)
-            pil2 = random.randrange(1,5)
-            pil3 = random.randrange(1,5)
-            pil4 = random.randrange(1,5)
-        if (pil2 == pil1):
-            pil2 = random.randrange(1,5)
-        while ((pil3 == pil2) or (pil3 == pil1)):
-            pil3 = random.randrange(1,5)
-        while ((pil4 == pil3) or (pil4 == pil2) or (pil4 == pil1)):
-            pil4 = random.randrange(1,5)
-
-        if (pil1 == 1):
-            pilihan1 = "1. " + str(self.sol1)
-            self.user_solution = 1
-        elif (pil1 == 2):
-            pilihan1 = "1. " + str(self.sol2)
-        elif (pil1 == 3):
-            pilihan1 = "1. " + str(self.sol3)
-        else:
-            pilihan1 = "1. " + str(self.sol4)
-
-        if (pil2 == 1):
-            pilihan2 = "2. " + str(self.sol1)
-            self.user_solution = 2 
-        elif (pil2 == 2):
-            pilihan2 = "2. " + str(self.sol2)
-        elif (pil2 == 3):
-            pilihan2 = "2. " + str(self.sol3)
-        else:
-            pilihan2 = "2. " + str(self.sol4)
-
-        if (pil3 == 1):
-            pilihan3 = "3. " + str(self.sol1)
-            self.user_solution = 3
-        elif (pil3 == 2):
-            pilihan3 = "3. " + str(self.sol2)
-        elif (pil3 == 3):
-            pilihan3 = "3. " + str(self.sol3)
-        else:
-            pilihan3 = "3. " + str(self.sol4)
-
-        if (pil4 == 1):
-            pilihan4 = "4. " + str(self.sol1)
-            self.user_solution = 4
-        elif (pil4 == 2):
-            pilihan4 = "4. " + str(self.sol2)
-        elif (pil4 == 3):
-            pilihan4 = "4. " + str(self.sol3)
-        else:
-            pilihan4 = "4. " + str(self.sol4)
-
-        self.pushButton_2.setText(_translate("MainWindow",pilihan1, None))
-        self.pushButton_3.setText(_translate("MainWindow",pilihan2, None))
-        self.pushButton_4.setText(_translate("MainWindow",pilihan3, None))
-        self.pushButton_5.setText(_translate("MainWindow",pilihan4, None))
+        
        
 
     def Time(self): #Function to compare current time with set time 
@@ -191,7 +155,7 @@ class Ui_MainWindow(object):
         self.current_m = int (strftime("%M"))
 
         if (self.current_h == self.alarm_h) and (self.current_m == self.alarm_m) and ((int(strftime("%S"))%15) == 0): #if the both time match 
-            print("ALARM ON!!!!!")
+            
             
             message1 = " The time is " + str(self.alarm_h) + " : " + str(self.alarm_m) + " on " + strftime("%A")
              
@@ -199,22 +163,109 @@ class Ui_MainWindow(object):
             #espeak.synth (message) #speak the message through audio jack
             correct = False
             while (correct == False):
+                print("ALARM ON!!!!!")
+                #os.system('mplayer alarm2.mp3') 
+                subprocess.call(['mplayer alarm2.mp3'], shell=True) 
                 number_one = random.randrange(1, 21)
                 number_two = random.randrange(1, 21)
                 problem = str(number_one) + " + " + str(number_two)
                 self.solution = number_one + number_two
                 self.label_2.setText(_translate("MainWindow",problem, None)) #display the message on GUI screen  
                 
-                self.get_user_solution()
+                #self.get_user_solution()
+                self.sol1 = self.solution
+                self.sol2 = self.solution + random.randrange(1,10)
+                self.sol3 = self.solution - random.randrange(1,10)
+                self.sol4 = self.solution - random.randrange(1,10)
+                while (self.sol4 == self.sol3 ):
+                    self.sol4 = self.solution - random.randrange(1,10)
+                    self.pil1 = random.randrange(1,5)
+                    
+                    self.pil2 = random.randrange(1,5)
+                    #global self.pil2
+                    
+                    self.pil3 = random.randrange(1,5)
+                    #global self.pil3
+                    
+                    self.pil4 = random.randrange(1,5)
+                    #global self.pil4
+
+                if (self.pil2 == self.pil1):
+
+                    self.pil2 = random.randrange(1,5)
+                while ((self.pil3 == self.pil2) or (self.pil3 == self.pil1)):
+                    self.pil3 = random.randrange(1,5)
+                while ((self.pil4 == self.pil3) or (self.pil4 == self.pil2) or (self.pil4 == self.pil1)):
+                    self.pil4 = random.randrange(1,5)
+
+                if (self.pil1 == 1):
+                    pilihan1 = "1. " + str(self.sol1)
+                    self.user_solution = 1
+                elif (self.pil1 == 2):
+                    pilihan1 = "1. " + str(self.sol2)
+                elif (self.pil1 == 3):
+                    pilihan1 = "1. " + str(self.sol3)
+                else:
+                    pilihan1 = "1. " + str(self.sol4)
+
+                if (self.pil2 == 1):
+                    pilihan2 = "2. " + str(self.sol1)
+                    self.user_solution = 2 
+                elif (self.pil2 == 2):
+                    pilihan2 = "2. " + str(self.sol2)
+                elif (self.pil2 == 3):
+                    pilihan2 = "2. " + str(self.sol3)
+                else:
+                    pilihan2 = "2. " + str(self.sol4)
+
+                if (self.pil3 == 1):
+                    pilihan3 = "3. " + str(self.sol1)
+                    self.user_solution = 3
+                elif (self.pil3 == 2):
+                    pilihan3 = "3. " + str(self.sol2)
+                elif (self.pil3 == 3):
+                    pilihan3 = "3. " + str(self.sol3)
+                else:
+                    pilihan3 = "3. " + str(self.sol4)
+
+                if (self.pil4 == 1):
+                    pilihan4 = "4. " + str(self.sol1)
+                    self.user_solution = 4
+                elif (self.pil4 == 2):
+                    pilihan4 = "4. " + str(self.sol2)
+                elif (self.pil4 == 3):
+                    pilihan4 = "4. " + str(self.sol3)
+                else:
+                    pilihan4 = "4. " + str(self.sol4)
+
+                self.pushButton_2.setText(_translate("MainWindow",pilihan1, None))
+                self.pushButton_3.setText(_translate("MainWindow",pilihan2, None))
+                self.pushButton_4.setText(_translate("MainWindow",pilihan3, None))
+                self.pushButton_5.setText(_translate("MainWindow",pilihan4, None))
                 print("Enter your answer")
-                self.input = input("Jawabannya? ")
+                #self.input = input("Jawabannya? ")
+                #time.sleep(10)
+                self.btn1pushed = False
+                self.btn2pushed = False
+                self.btn3pushed = False
+                self.btn4pushed = False
+                i = 0
+                while (( self.btn1pushed != True ) and ( self.btn2pushed != True ) and ( self.btn3pushed != True ) and ( self.btn4pushed != True )):
+                   # not doing anything                                                                                                                                                                                                   
+                   if ( i % 100000 == 0 ):
+                       print("Waiting for user to push button")
+                   QtCore.QCoreApplication.processEvents()
+                   i += 1;
+
+                
+                
                 if self.user_solution == self.input:
                     print("Correct.")
                     correct = True
                 else:
                     print("Incorrect.")
                     correct = False
-            #os.system('mplayer alarm2.mp3') 
+            
 
             time.sleep(1)
             
@@ -229,22 +280,30 @@ class Ui_MainWindow(object):
 
         message = "Alarm is set at " + str(self.alarm_h) + " hours " + str(self.alarm_m) + " minutes"
         self.label.setText(_translate("MainWindow", message, None)) #display the message on GUI screen  
-        espeak.synth (message) #speak the message through audio jack 
+        #espeak.synth (message) #speak the message through audio jack 
 
     def button_pressed_1(self):
+        #lobal self.btn1pushed
+        self.btn1pushed = True
+        print("Button 1 pushed")
         self.input = 1
     def button_pressed_2(self):
+        #global self.btn2pushed
+        self.btn2pushed = True
+        print("Button 2 pushed")
         self.input = 2
     def button_pressed_3(self):
+        #global self.btn3pushed
+        self.btn3pushed = True
+        print("Button 3 pushed")
         self.input = 3
     def button_pressed_4(self):
+        #global btn4pushed
+        self.btn4pushed = True
+        print("Button 4 pushed")
         self.input = 4
-        
-        
-
-    d
-
-        
+   
+           
 
 if __name__ == "__main__": #main function 
     
